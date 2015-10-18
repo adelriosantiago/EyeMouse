c241833cac7ee47133b14800dcc882fec14c98b1
Alejandro del Rio <adelriosantiago@gmail.com>
2015-10-17 22:49:30 -0600

    Add first sketch

---
+++ EyeTracking/EyeTracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeTracking
+{
+    public class EyeTracker
+    {
+        public int TrackImage(int a, int b) {
+            //TODO
+            int result = a + b;
+
+            return result;
+        }
+
+        public int SetIrisRadius(int a) {
+            //TODO
+            return -1;
+        }
+
+        public int DetectIris(int a) {
+            //TODO
+            return -1;
+        }
+
+    }
+}
